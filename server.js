de9e6a6c0549292c83baaef4b183eebb2cab2e85
Terence Yau <tyauzer@hotmail.com>
2019-02-01 00:05:19 +0000

    testing twilio

--- server.js
+++ server.js
@@ -16,6 +16,11 @@ const knexLogger  = require('knex-logger');
 
 // Seperated Routes for each Resource
 const foodsRoutes = require("./routes/foods");
+const accountSid = 'AC8d51779e1b2ffff782ba05b0c8265db4';
+const authToken = '00142b9ccbafd8ea41cad4428d9d09d0';
+
+// require the Twilio module and create a REST client
+const client = require('twilio')(accountSid, authToken);
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
@@ -45,6 +50,21 @@ app.get("/", (req, res) => {
 
 app.get("/menu", (req, res) => {
   res.render("menu");
+});
+
+app.post("/menu", (req, res) => {
+  const client = require('twilio')(accountSid, authToken);
+
+client.messages.create(
+  {
+    to: '+16047156043',
+    from: '+16042108661',
+    body: 'This is the twilio test!!',
+  },
+  (err, message) => {
+    console.log(message.sid);
+  }
+);
 });
 
 app.get("/confirmation", (req, res) => {
